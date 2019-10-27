5696a26f1fc9c74af2aa650660afbfaa159f9b06
Mario <48070830+mjcarranza@users.noreply.github.com>
2019-10-26 23:20:11 -0600

    Add files via upload

--- src/TextFinder.java
+++ src/TextFinder.java
@@ -1,162 +1,150 @@
 /**
- *
- * @author Mario
+ * Importación de clases necesarias.
  */
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import javax.swing.JLabel;
-public class TextFinder extends javax.swing.JFrame {
+import javax.swing.JPopupMenu;
 
+/**
+ *Clase principal del proyecto Text Finder.
+ * @author Mario
+ */
+public class TextFinder extends javax.swing.JFrame {
+    private JLabel label;
+    private JPopupMenu popUp;
     /**
-     * Creates new form TextFinder
+     * Constructor de la clase.
      */
     public TextFinder() {
         initComponents();
     }
-    
+    /**
+    * Inicialización de los componentes de la clase con sus respectivos diseños.
+    */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jRadioButton1 = new javax.swing.JRadioButton();
-        jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        PanelPrincipal = new javax.swing.JPanel();
+        busqueda = new javax.swing.JButton();
         Buscador = new java.awt.TextField();
-        jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
-        jButton2 = new javax.swing.JButton();
+        agregar = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox();
+        Panel1 = new javax.swing.JPanel();
 
         jRadioButton1.setText("jRadioButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 153, 255));
-        jPanel1.setLayout(null);
+        PanelPrincipal.setBackground(new java.awt.Color(0, 153, 255));
+        PanelPrincipal.setLayout(null);
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lupa.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        busqueda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lupa.png"))); // NOI18N
+        busqueda.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                busquedaActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(730, 20, 40, 30);
+        PanelPrincipal.add(busqueda);
+        busqueda.setBounds(620, 20, 40, 30);
 
         Buscador.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        Buscador.setText("TEXT");
+        Buscador.setText("Buscar en biblioteca");
         Buscador.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BuscadorActionPerformed(evt);
             }
         });
-        jPanel1.add(Buscador);
-        Buscador.setBounds(140, 20, 590, 30);
+        PanelPrincipal.add(Buscador);
+        Buscador.setBounds(20, 20, 590, 30);
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 905, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("NOMBRE", jPanel2);
-
-        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 905, Short.MAX_VALUE)
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("FECHA DE CREACIÓN", jPanel3);
-
-        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 905, Short.MAX_VALUE)
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 372, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("TAMAÑO", jPanel4);
-
-        jPanel1.add(jTabbedPane1);
-        jTabbedPane1.setBounds(10, 60, 910, 400);
-
-        jButton2.setText("Biblioteca");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        agregar.setText("AGREGAR");
+        agregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                agregarActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2);
-        jButton2.setBounds(823, 20, 90, 23);
+        PanelPrincipal.add(agregar);
+        agregar.setBounds(823, 20, 90, 30);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Nombre", "Fecha", "Tamaño" }));
+        PanelPrincipal.add(jComboBox1);
+        jComboBox1.setBounds(680, 20, 90, 30);
+
+        Panel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout Panel1Layout = new javax.swing.GroupLayout(Panel1);
+        Panel1.setLayout(Panel1Layout);
+        Panel1Layout.setHorizontalGroup(
+            Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 890, Short.MAX_VALUE)
+        );
+        Panel1Layout.setVerticalGroup(
+            Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+
+        PanelPrincipal.add(Panel1);
+        Panel1.setBounds(20, 60, 890, 400);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 930, Short.MAX_VALUE)
+            .addComponent(PanelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 930, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
+            .addComponent(PanelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        File carpeta = new File("C:/Users/Mario/Desktop/Mario");
-        String l;
-        if(carpeta.isDirectory()){
-            for(int i = 0; i < carpeta.listFiles().length; i++){
-                l = carpeta.listFiles()[i].toString();
-                if(l.contains(Buscador.getText())){
-                    System.out.println(l);
-                }
-            }
-        }
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void BuscadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BuscadorActionPerformed
+    /**
+     * En este método se definen las funciones del botón buscar de esta interfaz,
+     * identificado con la imagen de una lupa.
+     * @param evt 
+     */
+    private void busquedaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_busquedaActionPerformed
+        //codigo para que limpie el cuadro de texto.
         
-    }//GEN-LAST:event_BuscadorActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        VentanaOpciones ventana = new VentanaOpciones();
+        /**
+         * Llamamos al méotodo BuscarArchivo.
+         */
+        BuscarArchivo(new File("src\\Biblioteca"));
+    }//GEN-LAST:event_busquedaActionPerformed
+    /**
+     * En este método se definen las funciones del botón identificado como "Agregar",
+     * el cual nos envía a una interfaz nueva donde es posible buscar y agregar 
+     * archivos a nuestra biblioteca.
+     * @param evt 
+     */
+    private void agregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarActionPerformed
+        Agregar ventana = new Agregar();
         ventana.setVisible(true);
         this.setVisible(false);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_agregarActionPerformed
+    /**
+     * En este método se definen las funciones de la caja de texto en la parte superior de esta interfaz,
+     * que permite buscar palabras o frases dentro de todos los archivos disponibles en la biblioteca.
+     * @param evt 
+     */
+    private void BuscadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BuscadorActionPerformed
+        // codigo para que desaparezca el texto de la caja al dar click
+    }//GEN-LAST:event_BuscadorActionPerformed
 
     /**
-     * @param args the command line arguments
+     * Método principal de la clase.
+     * @param args 
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -164,34 +152,132 @@ public class TextFinder extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TextFinder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TextFinder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TextFinder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TextFinder.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TextFinder().setVisible(true);
-            }
+        /**
+         * Se crea y proyecta la interfaz.
+         */
+        java.awt.EventQueue.invokeLater(() -> {
+            new TextFinder().setVisible(true);
         });
     }
-
+    /**
+     * Declaración de las variables necesarias.
+     */
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private java.awt.TextField Buscador;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel Panel1;
+    private javax.swing.JPanel PanelPrincipal;
+    private javax.swing.JButton agregar;
+    private javax.swing.JButton busqueda;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
+    /**
+     * Método para buscar archivos dentro de la biblioteca de documentos.
+     * @param carpeta 
+     */
+    private void BuscarArchivo(File carpeta) {
+        String archivo;
+        int posY = 5;
+        for (File listFile : carpeta.listFiles()) {
+            try {
+                /**
+                 * Si el archivo pasado por parámetro es un directorio,
+                 * se busca dentro de él, si no lo es, entonces se crea una etiqueta con la dirección del 
+                 * archivo dentro del equipo
+                 */
+                if (listFile.isDirectory()) {
+                   BuscarArchivo(listFile);
+                } else {
+                    archivo = listFile.toString();
+                    if(archivo.contains(Buscador.getText())){
+                        label = new JLabel(archivo);
+                        label.setVisible(true);
+                        Panel1.add(label);
+                        label.setBounds(10, posY, 1000, 20);
+                        posY += 50;
+                        AbrirArchivo(label, archivo);
+                    }
+                    
+                }
+            }catch(Exception e){
+            }
+        }
+    }
+    
+    /**
+     * Método que se encaarga de abrir los archivos que el usuario decea.
+     * @param label
+     * @param archivo 
+     */
+    private void AbrirArchivo(JLabel label, String archivo) {
+    popUp = new JPopupMenu();
+    popUp.add(new AccionMenu("Abrir", popUp, archivo));
+    /**
+     * Se agrega un Listener a la etiqueta que contiene el archivo.
+     */
+    label.addMouseListener(new MouseAdapter() {
+        /**
+         * Método para validar si el mouse está sobre una etiqueta, si es así,
+         * el colo r del texto de la etiqueta cambia y también el cursor del mouse.
+         * @param e 
+         */
+        @Override
+        public void mouseEntered(MouseEvent e){
+            label.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            label.setForeground(Color.BLUE);
+        }
+        /**
+         * Método para validar si el mosuse se encuentra fuera de la etiqueta.
+         * @param e 
+         */
+        @Override
+        public void mouseExited(MouseEvent e){
+            label.setForeground(Color.BLACK);
+        }
+        /**
+         * Método que valida si el click derecho del mose es presionado.
+         * @param e 
+         */
+        @Override
+        public void mouseReleased(MouseEvent e){
+            muestraMenu(e);
+        }
+        /**
+         * Método para mostrar el menú al hacer click derecho sobre una etiqueta.
+         * @param e 
+         */
+        private void muestraMenu(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                popUp.setLocation(e.getLocationOnScreen());
+                popUp.setVisible(true);
+                }
+        }
+    });
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////       codigo para abrir documentos pdf, docx y txt desde java     ///////////////////////////////////////////////////////////////////
+    
+     /**
+     * Botón para agregar documento a la biblioteca
+     * @param e 
+     */
+    public void actionPerformed(ActionEvent e) {
+        popUp.setVisible(false);
+        try {
+            String direccion = "";// url
+            ProcessBuilder abrir = new ProcessBuilder();
+            abrir.command("cmd.exe","/c", direccion);
+            abrir.start();
+        } catch (Exception ex) {
+            System.out.println("Error de ejcucion");
+        }
+        
+    } 
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
