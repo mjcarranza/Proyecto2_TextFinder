4f80dea289994c74671b72960199742c96d261ca
Mario <48070830+mjcarranza@users.noreply.github.com>
2019-10-27 22:36:46 -0600

    Add files via upload

--- src/BinaryNode.java
+++ src/BinaryNode.java
@@ -12,54 +12,33 @@ public class BinaryNode {
      * Variables.
      */
     public String palabra;
-    public ArrayList<String> ocurrencias, direcciones;
+    public ArrayList<String> ocurrencias, direcciones, nombres, fechas;
+    public ArrayList<Integer> sizes;
     public BinaryNode left;
     public BinaryNode right;
+    
     /**
      * Método constrctor de la clase.
-     * @param palabra
-     * @param ocurrencia
-     * @param direcciones 
+     * @param palabra // Palabra a insertar.
+     * @param ocurrencia // lista de ocurrencias.
+     * @param direcciones // lista de direcciones de aerchivos.
+     * @param fechas // lista de fechas de cada archivo.
+     * @param nombres // lista de nombres de cada archivo.
+     * @param sizes // lista de los tamaños de cada archivo.
      */
-    public BinaryNode(String palabra, ArrayList<String> ocurrencia, ArrayList<String> direcciones) {
-        this(palabra, ocurrencia, direcciones, null, null);
+    public BinaryNode(String palabra, ArrayList<String> ocurrencia, ArrayList<String> direcciones, ArrayList<String> nombres, ArrayList<String> fechas, ArrayList<Integer> sizes) {
+        this(palabra, ocurrencia, direcciones, nombres, fechas, sizes, null, null);
     }
-    
-    public BinaryNode(String palabra, ArrayList<String> ocurrencias, ArrayList<String> direcciones, BinaryNode left, BinaryNode right) {
+
+    public BinaryNode(String palabra, ArrayList<String> ocurrencias, ArrayList<String> direcciones, ArrayList<String> nombres, ArrayList<String> fechas, ArrayList<Integer> sizes, BinaryNode left, BinaryNode right) {
         this.palabra = palabra;
         this.ocurrencias = ocurrencias;
         this.direcciones = direcciones;
+        this.nombres = nombres;
+        this.fechas = fechas;
+        this.sizes = sizes;
         this.left = left;
         this.right = right;
     }
-    /**
-     * Método para obtener las ocurrencias de un nodo.
-     * @return 
-     */
-    public ArrayList<String> getOcurrencias() {
-        return ocurrencias;
-    }
-    /**
-     * Método para obtener la direcció de un archivo dentro de un nodo.
-     * @return 
-     */
-    public ArrayList<String> getDireccion() {
-        return direcciones;
-    }
-    /**
-     * Método para establecer la lista de ocurrencias de una palabra dentro de un nodo.
-     * @param ocurrencias 
-     */
-    public void setOcurrencias(ArrayList<String> ocurrencias) {
-        this.ocurrencias = ocurrencias;
-    }
-    /**
-     * Método para establecer la direccion del archivo donde se encuentra la palabra.
-     * @param direcciones 
-     */
-    public void setDirecciones(ArrayList<String> direcciones) {
-        this.direcciones = direcciones;
-    }
-    
     
 }
